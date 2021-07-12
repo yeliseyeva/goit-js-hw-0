f70151ad6b3a7ae0ff013c3779ad358e0ac60f33
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-12 15:28:35 +0300

    prettierc

--- gallery.js
+++ gallery.js
@@ -1,14 +1,15 @@
-import galleryItems from './app.js'
+import galleryItems from './app.js';
 
 const galleryList = document.querySelector('.js-gallery');
 const lightbox = document.querySelector('.js-lightbox');
 const lightboxImage = document.querySelector('.lightbox__image');
-const btnClose = document.querySelector('.lightbox__button')
-const overlay = document.querySelector('.lightbox__overlay')
+const btnClose = document.querySelector('.lightbox__button');
+const overlay = document.querySelector('.lightbox__overlay');
 
 //Разметка
 
-const createGalleryMarkUp = galleryItems.map(({preview, original, description}) => {
+const createGalleryMarkUp = galleryItems.map(
+  ({ preview, original, description }) => {
     return `<li class="gallery__item">
                 <a class="gallery__link" href="${original}">
                     <img class="gallery__image" 
@@ -16,42 +17,43 @@ const createGalleryMarkUp = galleryItems.map(({preview, original, description}) 
                     data-source="${original}" 
                     alt="${description}"/>
                 </a>
-            </li>`
-});
+            </li>`;
+  }
+);
 
 galleryList.insertAdjacentHTML('afterbegin', createGalleryMarkUp.join(''));
 
 //Событие клика
 
-galleryList.addEventListener('click', (event) => {
-    event.preventDefault();
-    if(event.target.nodeName !== 'IMG'){
-        return;
-    };
-    
-    const imageSource = event.target.dataset.source;
-    const imageAlt = event.target.alt;
+galleryList.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
 
-    lightbox.classList.add('is-open');
+  const imageSource = event.target.dataset.source;
+  const imageAlt = event.target.alt;
 
-    lightboxImage.src = imageSource;
-    lightboxImage.alt = imageAlt;
-})
+  lightbox.classList.add('is-open');
 
-btnClose.addEventListener('click', (event) => {
-    lightbox.classList.remove('is-open');
-    lightboxImage.src = '';
-    lightboxImage.alt = '';
-})
+  lightboxImage.src = imageSource;
+  lightboxImage.alt = imageAlt;
+});
+
+btnClose.addEventListener('click', event => {
+  lightbox.classList.remove('is-open');
+  lightboxImage.src = '';
+  lightboxImage.alt = '';
+});
 
 overlay.addEventListener('click', event => {
-    lightbox.classList.remove('is-open');
-})
+  lightbox.classList.remove('is-open');
+});
 
 document.addEventListener('keydown', event => {
-    if (lightbox.classList.contains('is-open')){
-        if(event.key ===  'Escape'){
-            lightbox.classList.remove('is-open');
-        }
+  if (lightbox.classList.contains('is-open')) {
+    if (event.key === 'Escape') {
+      lightbox.classList.remove('is-open');
     }
-})
+  }
+});
