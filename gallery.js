89bd26d186c9a0bc052a2515bcc6bf04f41e33c1
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-12 14:28:04 +0300

    событие клика

--- gallery.js
+++ gallery.js
@@ -1,17 +1,32 @@
-import app from './app.js'
-
-const createGalleryMarkUp = ({preview, original, description}) => {
-    return `<li class="gallery__item">
-                <a class="gallery__link" href="${original}">
-                    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-                </a>
-            </li>`
-}
-
-// console.log(createGalleryMarkUp)
+import galleryItems from './app.js'
 
 const galleryList = document.querySelector('.js-gallery');
-// console.log(galleryList)
+const lightbox = document.querySelector('.js-lightbox');
 
-galleryList.insertAdjacentHTML = createGalleryMarkUp;
-console.log(galleryList.insertAdjacentHTML)
+//Разметка
+
+const createGalleryMarkUp = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" 
+                    src="${preview}" 
+                    data-source="${original}" 
+                    alt="${description}"/>
+                </a>
+            </li>`
+});
+
+galleryList.insertAdjacentHTML('afterbegin', createGalleryMarkUp.join(''));
+
+//Событие клика
+
+galleryList.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    };
+
+    lightbox.classList.add('is-open');
+    
+
+})
