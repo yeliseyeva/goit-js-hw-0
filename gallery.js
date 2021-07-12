4e4b0cbd3a4a3b95f33f8b13d02d8c8663adcc74
yeliseyeva <74910968+yeliseyeva@users.noreply.github.com>
2021-07-12 15:20:13 +0300

    закрытие по клавише

--- gallery.js
+++ gallery.js
@@ -2,6 +2,9 @@ import galleryItems from './app.js'
 
 const galleryList = document.querySelector('.js-gallery');
 const lightbox = document.querySelector('.js-lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+const btnClose = document.querySelector('.lightbox__button')
+const overlay = document.querySelector('.lightbox__overlay')
 
 //Разметка
 
@@ -25,8 +28,30 @@ galleryList.addEventListener('click', (event) => {
     if(event.target.nodeName !== 'IMG'){
         return;
     };
+    
+    const imageSource = event.target.dataset.source;
+    const imageAlt = event.target.alt;
 
     lightbox.classList.add('is-open');
-    
 
+    lightboxImage.src = imageSource;
+    lightboxImage.alt = imageAlt;
+})
+
+btnClose.addEventListener('click', (event) => {
+    lightbox.classList.remove('is-open');
+    lightboxImage.src = '';
+    lightboxImage.alt = '';
+})
+
+overlay.addEventListener('click', event => {
+    lightbox.classList.remove('is-open');
+})
+
+document.addEventListener('keydown', event => {
+    if (lightbox.classList.contains('is-open')){
+        if(event.key ===  'Escape'){
+            lightbox.classList.remove('is-open');
+        }
+    }
 })
